2e4d23100c34b6d3566b3d18f7afd0284742cd9a
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-29 10:19:53 +0530

    First Console APP

---
+++ Course 1/Week 1/FirstApp/FirstApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstApp
+{
+    /// <summary>
+    /// First Console App lecture code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// First Console App lecture code
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // print supportive message
+            Console.WriteLine("Hello, n00b!");
+        }
+    }
+}
