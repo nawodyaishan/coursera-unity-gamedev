12938a38cae8f97e37c5b83eb978d1ca383be9f2
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-29 15:29:43 +0530

    Float

---
+++ Course 1/Week 2/Exercise5/Assets/Scripts/PrintPercent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Exercise 5 Solution
+/// </summary>
+public class PrintPercent : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+	{
+        // set values
+        const int MaxScore = 100;
+        int score = 50;
+
+        // calculate and print percent
+        float percent = ((float)score / MaxScore) * 100;
+        print("Percent: " + percent + "%");
+    }
+}
---
+++ Course 1/Week 2/FloatingPointDataTypes/FloatingPointDataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FloatingPointDataTypes
+{
+    class Program
+    {
+        /// <summary>
+        /// Demonstarting Floating Point Data Types
+        /// </summary>
+        /// <param name="args">command-line arg</param>
+        static void Main(string[] args)
+        {
+
+            int score = 100;
+            int totalSecondsPlayed = 1000;
+
+            float pointsPerSecond = (float) score / (float) totalSecondsPlayed;
+            Console.WriteLine("Points per second: {0}", pointsPerSecond);
+
+            double firstNumber = 34.567839023;
+            double secondNumber = 8923.1234857;
+            double thirdNumber = 3456.09182371;
+            double fourthNumber = 3456.09182371;
+
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+            Console.WriteLine(thirdNumber);
+            Console.WriteLine(fourthNumber);
+        }
+    }
+}
