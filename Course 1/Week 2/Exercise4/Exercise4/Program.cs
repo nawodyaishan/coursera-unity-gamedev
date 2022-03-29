680ce8395cb41a5f94af85ea2445dd730ca9f6b5
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-29 15:19:24 +0530

    Week Integer

---
+++ Course 1/Week 2/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise4
+{
+    /// <summary>
+    /// Exercise 4 Solution
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Calculates altitude change between two locations
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // get locations
+            Console.Write("Enter altitude at first location:  ");
+            int firstAltitude = int.Parse(Console.ReadLine());
+            Console.Write("Enter altitude at second location: ");
+            int secondAltitude = int.Parse(Console.ReadLine());
+
+            // calculate and print altitude change
+            int altitudeChange = secondAltitude - firstAltitude;
+            Console.WriteLine();
+            Console.WriteLine("Altitude change: " + altitudeChange);
+        }
+    }
+}
---
+++ Course 1/Week 2/Week2Int/Week2Int/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week2Int {
+    /// <summary>
+    /// Integer Data Types Lecture Code
+    /// </summary>
+    internal class Program {
+        /// <summary>
+        /// Demonstrating Integer Data Types
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        
+        static void Main(string[] args) {
+            // Calculating second and minutes
+            int totalSecondsPlayed = 100;
+            int totalMinutesPlayed = totalSecondsPlayed / 60;
+            
+            // print minutes d second played
+            Console.WriteLine("" + totalMinutesPlayed + " minutes and " + (totalSecondsPlayed - (totalMinutesPlayed*60)) + " seconds");
+                    }
+    }
+}
