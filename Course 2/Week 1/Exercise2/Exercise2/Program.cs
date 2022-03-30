68e6b284342500a2a7c83eb104c53c3fd3179747
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-03-30 18:02:43 +0530

    Exercise 3

---
+++ Course 2/Week 1/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Exercise2
+{
+    /// <summary>
+    /// Exercise 2 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Gets user menu choice and prints message
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // print menu
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 – New Game");
+            Console.WriteLine("2 – Load Game");
+            Console.WriteLine("3 – Options");
+            Console.WriteLine("4 – Quit");
+            Console.WriteLine("**************");
+            Console.WriteLine();
+
+            // prompt for and get choice
+            Console.Write("Enter your choice (1, 2, 3 or 4): ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            // print message using if statement
+            if (choice == 1)
+            {
+                Console.WriteLine("Creating new game ...");
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Loading game ...");
+            }
+            else if (choice == 3)
+            {
+                Console.WriteLine("Setting options ...");
+            }
+            else if (choice == 4)
+            {
+                Console.WriteLine("Quitting ...");
+            }
+            else
+            {
+                Console.WriteLine("That's not a valid menu choice!");
+            }
+
+            // print message using switch statement
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Creating new game ...");
+                    break;
+                case 2:
+                    Console.WriteLine("Loading game ...");
+                    break;
+                case 3:
+                    Console.WriteLine("Setting options ...");
+                    break;
+                case 4:
+                    Console.WriteLine("Quitting ...");
+                    break;
+                default:
+                    Console.WriteLine("That's not a valid menu choice!");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Course 2/Week 1/Exercise3/Assets/Scripts/Resizer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Resizes the game object
+/// </summary>
+public class Resizer : MonoBehaviour
+{
+    // timer support
+    const float TotalResizeSeconds = 1;
+    float elapsedResizeSeconds = 0;
+
+    // resizing control
+    const float ScaleFactorPerSecond = 1;
+    int scaleFactorSignMultiplier = 1;
+
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+        // resize the game object
+        Vector3 newScale = transform.localScale;
+        newScale.x += scaleFactorSignMultiplier * ScaleFactorPerSecond * Time.deltaTime;
+        newScale.y += scaleFactorSignMultiplier * ScaleFactorPerSecond * Time.deltaTime;
+        transform.localScale = newScale;
+
+        // update timer and check if it's done
+        elapsedResizeSeconds += Time.deltaTime;
+        if (elapsedResizeSeconds >= TotalResizeSeconds) {
+
+            // reset timer and start resizing the game object
+            // in the opposite direction
+            elapsedResizeSeconds = 0;
+            scaleFactorSignMultiplier *= -1;
+        }
+	}
+}
