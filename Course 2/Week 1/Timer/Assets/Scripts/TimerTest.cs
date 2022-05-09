310918a8e55e12f050ce8a96a8ce2086e28f195a
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-09 12:33:44 +0530

    Timer

---
+++ Course 2/Week 1/Timer/Assets/Scripts/TimerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script to test the timer class
+/// </summary>
+public class TimerTest : MonoBehaviour
+{
+	// test object
+	Timer timer;
+
+	// time measurement
+	float startTime;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+		// create and run timer
+		timer = gameObject.AddComponent<Timer>();
+		timer.Duration = 3;
+		timer.Run ();
+
+		// save start time
+		startTime = Time.time;
+	}
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+		// check for timer just finished
+		if (timer.Finished)
+        {
+			float elapsedTime = Time.time - startTime;
+			Debug.Log("Timer ran for " + elapsedTime + " seconds");
+
+			// save start time and restart timer
+			startTime = Time.time;
+			timer.Run();
+		}
+	}
+}
