ffd105e7ee7f0834da91b543ee3c8cb3d0f125b8
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-01 23:23:36 +0530

    add assignment 1

---
+++ Course 2/Week 1/ProgrammingAssignment1/ProgrammingAssignment1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace ProgrammingAssignment1
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 1
+    /// </summary>
+    class Program
+    {
+        // number to classify
+        static int number;
+
+        /// <summary>
+        /// Programming Assignment 1
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+                // extract number from string
+                GetInputValueFromString(input);
+
+                // Add your code between this comment
+                // and the comment below. You can of
+                // course add more space between the
+                // comments as needed
+
+
+                
+                if (number % 2 == 0)
+                {
+                    Console.Write("e");
+                    Console.Write(" ");
+                
+                    if (number > 0)
+                    {
+                        Console.Write("1");
+                    }
+                    else if (number < 0)
+                    {
+                        Console.Write("-1");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                    
+                }
+                else if (number % 2 == 0)
+                {
+                    Console.Write("o");
+                    Console.Write(" ");
+                
+                    if (number > 0)
+                    {
+                        Console.Write("1");
+                    }
+                    else if (number < 0)
+                    {
+                        Console.Write("-1");
+                    }
+                    else
+                    {
+                        Console.Write("0");
+                    }
+                }
+               
+               
+
+                // Don't add or modify any code below
+                // this comment
+                input = Console.ReadLine();
+            }
+        }
+        
+        /// <summary>
+        /// Extracts the number from the given input string
+        /// </summary>
+        /// <param name="input">input string</param>
+        static void GetInputValueFromString(string input)
+        {
+            number = int.Parse(input);
+        }
+    }
+}
