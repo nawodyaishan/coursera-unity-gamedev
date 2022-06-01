04a8fb27abfb2f34d13e4de9530166abfd8ad132
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-01 22:35:24 +0530

    Add RockOfAges

---
+++ Course 2/Week 1/RockOfAges/Assets/Scripts/Rock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A rock
+/// </summary>
+public class Rock : MonoBehaviour
+{
+	/// <summary>
+	/// Start is called before the first frame update
+	/// </summary>
+	void Start()
+	{
+        // apply impulse force to get game object moving
+        const float MinImpulseForce = 1f;
+        const float MaxImpulseForce = 2f;
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector2 direction = new Vector2(
+            Mathf.Cos(angle), Mathf.Sin(angle));
+        float magnitude = Random.Range(MinImpulseForce, MaxImpulseForce);
+        GetComponent<Rigidbody2D>().AddForce(
+            direction * magnitude,
+            ForceMode2D.Impulse);		
+	}
+
+    /// <summary>
+    /// Called when the rock becomes invisible
+    /// </summary>
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Course 2/Week 1/RockOfAges/Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A rock spawner
+/// </summary>
+public class RockSpawner : MonoBehaviour
+{
+    // needed for spawning
+    [SerializeField]
+    GameObject prefabRock;
+
+    // saved for efficiency
+    [SerializeField]
+    Sprite greenRockSprite;
+    [SerializeField]
+    Sprite magentaRockSprite;
+    [SerializeField]
+    Sprite whiteRockSprite;
+
+    // spawn control
+    const float SpawnDelaySeconds = 1;
+    Timer spawnTimer;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        // create and start timer
+        spawnTimer = gameObject.AddComponent<Timer>();
+        spawnTimer.Duration = SpawnDelaySeconds;
+        spawnTimer.Run();
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // check for time to spawn a new rock
+        if (spawnTimer.Finished &&
+            GameObject.FindGameObjectsWithTag("Rock").Length < 3)
+        {
+            SpawnRock();
+
+            // restart spawn timer
+            spawnTimer.Run();
+        }
+    }
+
+    /// <summary>
+    /// Spawns a new rock at the center of the window 
+    /// </summary>
+    void SpawnRock()
+    {
+        // create new rock
+        GameObject rock = Instantiate(prefabRock) as GameObject;
+        rock.transform.position = Vector3.zero;
+
+        // set random sprite for new rock
+        SpriteRenderer spriteRenderer = rock.GetComponent<SpriteRenderer>();
+        int spriteNumber = Random.Range(0, 3);
+        if (spriteNumber == 0)
+        {
+            spriteRenderer.sprite = greenRockSprite;
+        }
+        else if (spriteNumber == 1)
+        {
+            spriteRenderer.sprite = magentaRockSprite;
+        }
+        else
+        {
+            spriteRenderer.sprite = whiteRockSprite;
+        }
+    }
+}
