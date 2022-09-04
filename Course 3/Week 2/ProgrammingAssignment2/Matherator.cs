75d1a04cbf8d6b16cbe130fb3f6a1d3d6827a4aa
nawodyaishan <nawodyain@gmail.com>
2022-09-04 14:45:24 +0530

    Course 3 Week 2 Assignment

---
+++ Course 3/Week 2/ProgrammingAssignment2/Matherator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Provides a variety of numeric methods
+    /// </summary>
+    public class Matherator
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Matherator()
+        {
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Prints the numbers from 1 to 10
+        /// </summary>
+        public void PrintOneToTen()
+        {
+            PrintMToN(1, 10);
+        }
+
+        /// <summary>
+        /// Prints the numbers from m to n
+        /// </summary>
+        /// <param name="m">m</param>
+        /// <param name="n">n</param>
+        public void PrintMToN(int m, int n)
+        {
+            if (m < n)
+            {
+                for (int i = m; i <= n; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                (m, n) = (n, m);
+            }
+            Console.Write("\n");
+        }
+
+        /// <summary>
+        /// Returns the tenth even number, with 2 as the first even number
+        /// </summary>
+        /// <returns>tenth even number</returns>
+        public int GetTenthEvenNumber()
+        {
+            return GetNthEvenNumber(10);
+        }
+
+        /// <summary>
+        /// Returns the nth even number, with 2 as the first even number
+        /// </summary>
+        /// <param name="n">n</param>
+        /// <returns>nth even number</returns>
+        public int GetNthEvenNumber(int n)
+        {
+            var result = 2 * n;
+            return result;
+        }
+
+        #endregion
+    }
+}
